a8338e568f78c8736be51b40efa70850f640d42a
João Matheus Jinkings <joaomatheusjinkings@gmail.com>
2023-09-15 20:07:47 -0300

    entrega 5 finalizada.

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const barbearia = {
         { id: 4, tipo: "Moicano", valor: 10 },
         { id: 5, tipo: "Razor part", valor: 5 },
     ],
-  
+
     barbas: [
         { id: 1, tipo: "Capitão Jack", valor: 20 },
         { id: 2, tipo: "Van Dyke", valor: 20 },
@@ -16,30 +16,74 @@ const barbearia = {
     ],
     estaAberto: true,
 };
-  
-  
+
 function buscaCortePorId(id) {
+    for(let i = 0; i < barbearia.cortes.length; i++) {
+        if(id === barbearia.cortes[i].id) {
+            return barbearia.cortes[i];
+        }
+    }
+    
+    return "Corte não encontrado.";
 }
 
 function buscaBarbaPorId(id) {
+    for(let i = 0; i < barbearia.barbas.length; i++) {
+        if(id === barbearia.barbas[i].id) {
+            return barbearia.barbas[i];
+        }
+    }
+    
+    return "Barba não encontrado.";
 }
 
 function verificaStatusBarbearia() {
+    if(barbearia.estaAberto) {
+        return "Estamos abertos";
+    }
+
+    return "Estamos fechados";
 }
 
 function retornaTodosCortes() {
+    return barbearia.cortes;
 }
 
 function retornaTodasBarbas() {
+    return barbearia.barbas;
 }
 
 function criaPedido(nomeCliente, corteId, barbaId) {
+    const corte = buscaCortePorId(corteId);
+    const barba = buscaBarbaPorId(barbaId);
+
+    const pedido = {
+        nome: nomeCliente,
+        pedidoCorte: corte.tipo,
+        pedidoCortePreco: corte.valor,
+        pedidoBarba: barba.tipo,
+        pedidoBarbaPreco: barba.valor
+    }
+
+    return pedido;
 }
 
 function atualizarServico(lista, id, valor, tipo) {
+    for(let i = 0; i < lista.length; i++) {
+        let itemLista = lista[i];
+        if(itemLista.id === id) {
+            itemLista.valor = valor;
+            itemLista.tipo = tipo;
+        }
+    }
+
+    return lista;
 }
 
 function calculaTotal(pedido) {
+    let soma = pedido.pedidoCortePreco + pedido.pedidoBarbaPreco;
+    return soma;
 }
-  
-  
+
+
+
